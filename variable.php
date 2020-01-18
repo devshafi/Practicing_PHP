4a66b11466e444e4336f57d81ec46f6ab599c5e3
shafi <frshafi49@gmail.com>
2020-01-18 21:56:49 +0600

    added static variable example

--- variable.php
+++ variable.php
@@ -7,11 +7,43 @@ $name = "Shafi";
 echo "My name is $name <br>";
 
 // another way of concatinating variable in php
-echo "My name is".$name."!<br>";
+echo "My name is ".$name."!<br>";
 
 //variable concatination in declaring
 $age = 32;
-$description = "My age is " .$age. ". I like walking in the rain.";
+$description = "My age is " .$age. ". I like walking in the rain.<br>";
 echo "$description";
 
+
+//variable with global scope
+$color = "red";
+
+function readColor(){
+    // using color inside the function will generate an error
+    // echo "$color";  //undefined variable
+}
+// readColor();  //undefined variable
+
+// to access a global variable we have to use the keyword global
+$number = 50;
+function readGlobal(){
+    global $number;
+    echo "$number <br>";
+}
+readGlobal(); // no error
+
+
+// generally all the variable of a function are deleted after executing the functions
+// but if we want to save the variable after execution we have use the keyword statc
+
+function myStaticVaribale(){
+    static $number = 0;
+    echo "$number<br>";
+    $number ++;
+}
+
+// all the functions are using previous value of number
+myStaticVaribale();
+myStaticVaribale();
+myStaticVaribale();
 
