2b5198db5c33055a313c2441d5f29866db1d08e0
shafi <frshafi49@gmail.com>
2020-01-18 21:15:17 +0600

    added some more example

--- variable.php
+++ variable.php
@@ -1,3 +1,17 @@
 <?php
+
+// declaring variable
 $name = "Shafi";
-echo "My name is $name";
+
+// added variable in echo
+echo "My name is $name <br>";
+
+// another way of concatinating variable in php
+echo "My name is".$name."!<br>";
+
+//variable concatination in declaring
+$age = 32;
+$description = "My age is " .$age. ". I like walking in the rain.";
+echo "$description";
+
+
